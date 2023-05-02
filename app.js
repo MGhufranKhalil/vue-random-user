a8443356bdf4c47134fcfca4145bb524383e1216
MGhufranKhalil <ghufranrana95@gmail.com>
2023-05-03 00:17:24 +0500

    dynamic users

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
-const app = Vue.createApp({
-    template:`<h1>Hello World</h1>`,
+const app = Vue.createApp({ 
+    data() {
+       return{
+           firstName:'Ghufran',
+           lastName:'Khalil',
+           email:'ghufranrana95@gmail.com',
+           gender:'male',
+           picture:'https://randomuser.me/api/portraits/men/10.jpg',
+       }
+    },
+    methods: {
+        async getUser(){
+            const res = await fetch('https://randomuser.me/api')
+            const { results } = await res.json() 
+            this.firstName= results[0].name.first
+            this.lastName= results[0].name.last
+            this.email= results[0].email
+            this.gender= results[0].gender
+            this.picture=results[0].large
+        },
+    }
+
 });
 
 
